f020234b440cd438fae3cb8d2384ae839f302fe1
Engineer wanga <wanga.15065@students.kyu.ac.ke>
2026-01-16 14:01:39 +0300

    refine

--- loan/__init__.py
+++ loan/__init__.py
@@ -2,27 +2,33 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from loan.rdbms.database import Database
-from loan.rdbms.bootstrap import bootstrap  # optional: create tables on startup
+from loan.rdbms.bootstrap import bootstrap
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATA_DIR = os.getenv(
+    "DATA_DIR",
+    os.path.join(BASE_DIR, "..", "data")
+)
 
-db = Database(data_dir="data")  # ✅ persistent JSON DB
+db = Database(data_dir=DATA_DIR)
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'dev-secret-key'
+    secret_key = os.getenv("SECRET_KEY")
+    if not secret_key:
+        raise RuntimeError("❌ SECRET_KEY not set")
 
+    app.config["SECRET_KEY"] = secret_key
     bcrypt.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = "main.login"
-
-    # -----------------------------
-    # Bootstrap tables
-    # -----------------------------
     bootstrap(db)
-
     from .routes import main
     app.register_blueprint(main)
 
